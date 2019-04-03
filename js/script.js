1d94d6c8515eef2cd944b64cfa72ae61f2f945e2
Roman Jędras <romanjedras@o2.pl>
2019-04-03 15:56:15 +0200

    Add dom event load on ajax

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+(function () {
+    let url = 'http://api.icndb.com/jokes/random';
+
+    let button = document.getElementById('get-joke');
+
+
+    document.addEventListener('DOMContentLoaded',function () {
+        getJoke();
+    });
+
+    button.addEventListener('click', function(){
+        getJoke();
+    });
+
+    let paragraph = document.getElementById('joke');
+
+    function getJoke() {
+    let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', function(){
+            let response = JSON.parse(xhr.response);
+            paragraph.innerHTML = response.value.joke;
+        });
+        xhr.send();
+    }
+})();
